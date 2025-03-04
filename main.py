20e7cb1676baa4299772aad4b4fdd0b75a962f25
waveus <wav3us@gmail.com>
2025-03-04 18:01:10 +0100

    todo validating -f validating

--- main.py
+++ main.py
@@ -7,29 +7,23 @@ from enum import Enum
 class Quantity(Enum):
     NONE = 0
     ONE = 1
-    SOME = 2
+    ONEORMORE = 2
 
 class Arg:
-    def __init__(self, arg_name: str, flag: str, args_num: Quantity, help_message = ""):
+    def __init__(self, arg_name: str, flag: str, args_num: Quantity, default, help_message = ""):
         self.name = arg_name
         self.flag = flag
         self.args_num = args_num
         self.help = help_message
-
-        if args_num == Quantity.NONE:
-            self.value = False
-        elif args_num == Quantity.ONE:
-            self.value = ""
-        elif args_num == Quantity.SOME:
-            self.value = []
+        self.value = default
 
 class ArgsResolver:
     def __init__(self, arg_list: list[Arg] = None):
-        self.argList: list[Arg] = arg_list if arg_list is not None else []
+        self.arg_dict: dict[str, Arg] = {arg.flag: arg for arg in (arg_list or [])}
 
     def arg_resolve(self):
         
-        for arg_list in self.argList:
+        for arg_list in self.arg_dict.values():
             for i, arg_sys in enumerate(sys.argv[1:]):
                 if arg_list.flag == arg_sys:
                     if arg_list.args_num == Quantity.NONE:
@@ -39,31 +33,56 @@ class ArgsResolver:
                         if i + 1 < len(sys.argv[1:]) and not sys.argv[i + 2].startswith("-"):
                             arg_list.value = sys.argv[i+2]
                             break
-                    if arg_list.args_num == Quantity.SOME:
+                    if arg_list.args_num == Quantity.ONEORMORE:
                         j = 0
+                        arg_list.value.clear()
                         while i + j + 1 < len(sys.argv[1:]) and not sys.argv[i+ j + 2].startswith("-"):
-                            arg_list.value.append(sys.argv[i + j +2])
+                            arg_list.value.append(sys.argv[i + j + 2])
                             j = j + 1
                         break
+    def make_folder_file_paths(self):
+        possible_paths: list[str] = self.arg_dict["-p"].value
+        correct_paths: list[str] = []
+
+        for arg in possible_paths:
+            absolute_path = os.path.abspath(arg)
+            if os.path.exists(absolute_path) == True and os.path.isdir(absolute_path) == True: 
+
+                correct_paths.append(absolute_path)
+
+        if (len(correct_paths) == 0):
+            self.arg_dict["-p"].value = None
+        else:
+            possible_files: list[str] = self.arg_dict["-f"].value
+            correct_files: list[str] = []
+            for path in self.arg_dict["-p"].value:
+                
+            
+        
     
 args : list[Arg] = [
-    Arg("recursive", "-r", Quantity.NONE, "Flag that specifies whether the program should include subfolders when searching for duplicates."),
-    Arg("paths", "-f", Quantity.SOME, "Path of folders and files that should be scanned."),
-    Arg("hash", "-h", Quantity.SOME, "Specify which hash should be used to determine whether the file dyplicate exists.")
+    Arg("recursive", "-r", Quantity.NONE, False, "Flag that specifies whether the program should include subfolders when searching for duplicates."),
+    Arg("paths", "-p", Quantity.ONEORMORE, ["."], "Path of folders that should be scanned."),
+    Arg("hash", "-h", Quantity.ONE, "md5", "Specify which hash should be used to determine whether the file duplicate exists."),
+    Arg("files", "-f", Quantity.ONEORMORE, ["*"], "Name of the files that should be compared with every file in the '-p' folder or its subfolders if '-r' enabled")
 ]
 
 def main():
     resolver = ArgsResolver(args)
     resolver.arg_resolve()
-    print(resolver.argList[1].value)
-    
+    resolver.arg_dict["-p"].value = make_paths(resolver.arg_dict["-p"].value)
+    resolver.arg_dict["-f"].value = make_paths(resolver.arg_dict["-f"].value)
+    print(resolver.arg_dict["-r"].value)
+    print(resolver.arg_dict["-p"].value)
+    print(resolver.arg_dict["-h"].value)
+    print(resolver.arg_dict["-f"].value)
 
 def make_paths(possiblepaths: list[str]) -> Optional[list[str]]:
     
     sys_args: list[str] = possiblepaths
     user_path: list[str] = []
 
-    for arg in sys_args[1:]:
+    for arg in sys_args:
         absolute_path = os.path.abspath(arg)
         if os.path.exists(absolute_path) == True: 
             user_path.append(absolute_path)
@@ -73,11 +92,6 @@ def make_paths(possiblepaths: list[str]) -> Optional[list[str]]:
     
     return user_path
     
-def resolve_flags():
-    sys_args: list[str] = sys.argv
-    
-
-
 if __name__ == "__main__":
     main()
     
