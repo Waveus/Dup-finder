2de1e46d2dd79e6cb013598400282f067bb3e038
waveus <wav3us@gmail.com>
2025-03-04 01:46:57 +0100

    working args resolver

--- main.py
+++ main.py
@@ -14,26 +14,48 @@ class Arg:
         self.name = arg_name
         self.flag = flag
         self.args_num = args_num
-        self.enabled = False
         self.help = help_message
-        
+
         if args_num == Quantity.NONE:
-            self.value = None
+            self.value = False
         elif args_num == Quantity.ONE:
             self.value = ""
         elif args_num == Quantity.SOME:
             self.value = []
 
 class ArgsResolver:
-    def __init__(self, lista: list[Arg] = None):
-        self.argList: list[Arg] = None
-        print(type(self.argList))
+    def __init__(self, arg_list: list[Arg] = None):
+        self.argList: list[Arg] = arg_list if arg_list is not None else []
+
+    def arg_resolve(self):
+        
+        for arg_list in self.argList:
+            for i, arg_sys in enumerate(sys.argv[1:]):
+                if arg_list.flag == arg_sys:
+                    if arg_list.args_num == Quantity.NONE:
+                        arg_list.value = True
+                        break
+                    if arg_list.args_num == Quantity.ONE:
+                        if i + 1 < len(sys.argv[1:]) and not sys.argv[i + 2].startswith("-"):
+                            arg_list.value = sys.argv[i+2]
+                            break
+                    if arg_list.args_num == Quantity.SOME:
+                        j = 0
+                        while i + j + 1 < len(sys.argv[1:]) and not sys.argv[i+ j + 2].startswith("-"):
+                            arg_list.value.append(sys.argv[i + j +2])
+                            j = j + 1
+                        break
     
+args : list[Arg] = [
+    Arg("recursive", "-r", Quantity.NONE, "Flag that specifies whether the program should include subfolders when searching for duplicates."),
+    Arg("paths", "-f", Quantity.SOME, "Path of folders and files that should be scanned.")
+]
+
 def main():
-    userPaths = make_paths()
-    arg: list[Arg] = []
-    arg.append(Arg("recursive", "--r", Quantity.NONE, "Flag that specifies whether the program should include subfolders when searching for duplicates."))
-    arg.append(Arg("paths", "", Quantity.SOME, "Path of folders and files that should be scanned."))
+    resolver = ArgsResolver(args)
+    resolver.arg_resolve()
+    print(resolver.argList[1].value)
+    
 
 def make_paths() -> Optional[list[str]]:
     
